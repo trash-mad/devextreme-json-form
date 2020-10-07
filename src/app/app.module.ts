8b78103bc0113ce91c9d86a6d17f6c24fd49191f
Petr Tripolsky <tripolskypetr@gmail.com>
2020-10-07 15:29:24 +0300

    ngDoBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { MiniModule } from 'mini/mini.module';
-import { NgModule } from '@angular/core';
+import { Inject, Injector, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,24 @@ import { AppComponent } from './app.component';
     BrowserModule,
     MiniModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // tslint:disable-next-line: no-string-literal
+  providers: [{provide: APP_BASE_HREF, useValue: window['miniBaseHref']}],
+  entryComponents: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor(private injector: Injector) { }
+
+  /**
+   * Если осуществляется develop сборка - на странице будет <app-root>, взятый
+   * из шаблона index.html в папке src. В противном случае, по умолчанию ничего
+   * не произойдет, но инстанцирование <mini-web-component> будет так же доступно
+   */
+  ngDoBootstrap() {
+    const component: any = createCustomElement(AppComponent, { injector: this.injector });
+    if (document.querySelector('app-root')) {
+      customElements.define('app-root', component);
+    }
+  }
+
+}
